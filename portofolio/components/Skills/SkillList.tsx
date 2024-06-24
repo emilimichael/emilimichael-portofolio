fedb2ec299e7614be5094ca32550f1b80211792b
Emili Michael <emilimichael@outlook.com>
2024-06-24 17:27:29 +0100

    feat: Skills Section

---
+++ portofolio/components/Skills/SkillList.tsx
@@ -0,0 +1,33 @@
+import SkillItem, { SkillItemProps } from "./SkillsItem";
+import { skills, learning } from "./skillData";
+
+export default function SkillList() {
+  return (
+    <section className="flex flex-col items-center gap-10 mx-2">
+      <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">Skills and Tooling</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-6">
+        {skills.map((skill: SkillItemProps, index: number) => (
+          <SkillItem
+            key={index}
+            iconSrc={skill.iconSrc}
+            alt={skill.alt}
+            text={skill.text}
+          />
+        ))}
+      </div>
+      <h3 className="text-l md:text-xl lg:text-2xl font-semibold">
+        Currently Learning
+      </h3>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-2 lg:gap-6">
+        {learning.map((skill: SkillItemProps, index: number) => (
+          <SkillItem
+            key={index}
+            iconSrc={skill.iconSrc}
+            alt={skill.alt}
+            text={skill.text}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
